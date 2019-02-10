a671f7aaa9b1626d874771264afacd0aa93d1f6b
Leticia Muller <leticia.muller@sap.com>
2019-02-10 10:19:55 -0200

    added apiKey validation

--- src/main/java/com/challenge/idincrement/service/LoginService.java
+++ src/main/java/com/challenge/idincrement/service/LoginService.java
@@ -1,13 +1,10 @@
 package com.challenge.idincrement.service;
 
-import java.util.Objects;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.challenge.idincrement.exception.UnauthorizedException;
 import com.challenge.idincrement.model.ApiKey;
 import com.challenge.idincrement.model.User;
 import com.challenge.idincrement.repository.IdEntityRepository;
@@ -32,14 +29,8 @@ public class LoginService {
 		userRequestValidator.validateLoginData(user);
 		logger.info("Trying to log user {}", user.getEmail());
 		ApiKey apiKey = new ApiKey(idEntityRepository.findApiKeyByEmailPasswordAndTableName(user.getEmail(), user.getPassword(), user.getTableName()));
-		validate(apiKey.getApiKey());
+		userRequestValidator.validateApiKey(apiKey.getApiKey());
 		return apiKey;
 	}
 
-	private void validate(String apiKey) {
-		logger.info("Validating Api Key {}", apiKey);
-		if (Objects.isNull(apiKey) || apiKey.isEmpty()) {
-			throw new UnauthorizedException("User not found!");
-		}
-	}
 }
