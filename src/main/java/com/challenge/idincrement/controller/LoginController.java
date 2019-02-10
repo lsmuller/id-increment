55d29043a7d4aed551a697f14420b9c915a9190b
Leticia Muller <leticia.muller@sap.com>
2019-02-10 10:12:21 -0200

    update controller and service

--- src/main/java/com/challenge/idincrement/controller/LoginController.java
+++ src/main/java/com/challenge/idincrement/controller/LoginController.java
@@ -1,7 +1,5 @@
 package com.challenge.idincrement.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,16 +8,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.challenge.idincrement.exception.UnauthorizedException;
 import com.challenge.idincrement.model.ApiKey;
 import com.challenge.idincrement.model.User;
 import com.challenge.idincrement.service.LoginService;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/login")
 public class LoginController {
-
-	private Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
 
 	private LoginService loginService;
 
@@ -28,13 +23,9 @@ public class LoginController {
 		this.loginService = loginService;
 	}
 
-	@PostMapping("login")
+	@PostMapping
 	public ResponseEntity<ApiKey> login(@RequestBody User user) {
-		try {
-			return new ResponseEntity<>(loginService.login(user), HttpStatus.ACCEPTED);
-		} catch (UnauthorizedException e) {
-			return new ResponseEntity<>(new ApiKey(), HttpStatus.UNAUTHORIZED);
-		}
+		return new ResponseEntity<>(loginService.login(user), HttpStatus.ACCEPTED);
 	}
 
 }
--- src/main/java/com/challenge/idincrement/service/LoginService.java
+++ src/main/java/com/challenge/idincrement/service/LoginService.java
@@ -11,6 +11,7 @@ import com.challenge.idincrement.exception.UnauthorizedException;
 import com.challenge.idincrement.model.ApiKey;
 import com.challenge.idincrement.model.User;
 import com.challenge.idincrement.repository.IdEntityRepository;
+import com.challenge.idincrement.validation.UserRequestValidator;
 
 @Service
 public class LoginService {
@@ -19,12 +20,16 @@ public class LoginService {
 
 	private IdEntityRepository idEntityRepository;
 
+	private UserRequestValidator userRequestValidator;
+
 	@Autowired
-	public LoginService(IdEntityRepository idEntityRepository) {
+	public LoginService(IdEntityRepository idEntityRepository, UserRequestValidator userRequestValidator) {
 		this.idEntityRepository = idEntityRepository;
+		this.userRequestValidator = userRequestValidator;
 	}
 
 	public ApiKey login(User user) {
+		userRequestValidator.validateLoginData(user);
 		logger.info("Trying to log user {}", user.getEmail());
 		ApiKey apiKey = new ApiKey(idEntityRepository.findApiKeyByEmailPasswordAndTableName(user.getEmail(), user.getPassword(), user.getTableName()));
 		validate(apiKey.getApiKey());
