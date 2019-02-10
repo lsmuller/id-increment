e1a6086d56a775f19a01396b2d6bbd8f0089c890
Leticia Muller <leticia.muller@sap.com>
2019-02-10 09:37:58 -0200

    created repository

---
+++ src/main/java/com/challenge/idincrement/repository/IdEntityRepository.java
@@ -0,0 +1,13 @@
+package com.challenge.idincrement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.challenge.idincrement.entity.IdEntity;
+
+public interface IdEntityRepository extends JpaRepository<IdEntity, String> {
+
+	@Query("SELECT apiKey FROM IdEntity AS ie where ie.email = :email and ie.password = :password and ie.tableName = :tableName")
+	String findApiKeyByEmailPasswordAndTableName(@Param("email") String email, @Param("password") String password, @Param("tableName") String tableName);
+}
